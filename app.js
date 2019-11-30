cd5e2ca576b70a02a0c48e967a13273d2a154525
burninggun <willchengsters@gmail.com>
2019-11-30 12:56:49 -0800

    starting about page. adding routes to server

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 const path = require('path');
-app.use('/', express.static(path.join(__dirname, 'client')))
+const serveStatic = require('serve-static')
 
-app.listen(PORT, () => {
-    console.log('Server is listening to port: ', PORT)
-})
+
+
+app.use(serveStatic(path.join(__dirname, "client"), {index: false}))
 
 app.get('/', (req, res) => {
-    res.sendFile( "./index.html")
+    res.sendFile(path.join(__dirname, '/client', 'index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client', 'about.html'));
+    // res.sendFile('/about.html', {root: path.join(__dirname + '/client')});
 })
 
 app.get('/*', (req, res) => {
     res.redirect('/')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is listening to port: ', PORT)
 })
